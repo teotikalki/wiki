be152de23170e1b7bffab7a4139b740eafc943ff
anhvurz90 <anhvurz90@gmail.com>
2013-12-13 18:27:09 +0700

    WIKI-772 update indentation for TOC, add some properties for TOC dialog

--- wiki-webapp/src/main/webapp/eXoPlugins/toc/dialogs/toc.js
+++ wiki-webapp/src/main/webapp/eXoPlugins/toc/dialogs/toc.js
@@ -3,40 +3,69 @@ CKEDITOR.dialog.add( 'toc', function( editor ) {
         title: 'Table Of Content',
         minWidth: 200,
         minHeight: 100,
-	contents: [
+        contents: [
             {
                 id: 'info',
                 elements: [
                     // Dialog window UI elements.
-		    {
-			id: 'align',
-			type: 'select',
-			label: 'Align',
-			items: [
-			    [ editor.lang.common.notSet, '' ],
-			    [ editor.lang.common.alignLeft, 'left' ],
-			    [ editor.lang.common.alignRight, 'right' ],
-			    [ editor.lang.common.alignCenter, 'center' ]
-			],
-			setup: function( widget ) {
-			    this.setValue( widget.data.align );
-			},
-			commit: function( widget ) {
-			    widget.setData( 'align', this.getValue() );
-			}
-		    },
-		    {
-			id: 'width',
-			type: 'text',
-			label: 'Width',
-			width: '50px',
-			setup: function( widget ) {
-			    this.setValue( widget.data.width );
-			},
-			commit: function( widget ) {
-			    widget.setData( 'width', this.getValue() );
-			}
-		    }
+                    {
+                    	id: 'Depth',
+                    	type: 'text',
+                    	label: 'Depth',
+                    	width: '50px',
+                    	setup: function( widget ) {
+                    		this.setValue( widget.data.depth );
+                    	},
+                    	commit: function( widget ) {
+                    		widget.setData( 'depth', this.getValue() );
+                    	}
+                    },
+
+                    {
+                    	id: 'Numbered',
+                    	type: 'select',
+                    	label: 'Numbered',
+                    	items: [
+                	        [ 'Yes', 'Yes' ],
+                	        [ 'No', 'No' ]
+            	        ],
+            	        setup: function( widget ) {
+            	        	this.setValue( widget.data.numbered );
+            	        },
+            	        commit: function( widget ) {
+            	        	widget.setData( 'numbered', this.getValue() );
+            	        }
+                    },
+                    
+                    {
+                    	id: 'Scope',
+                    	type: 'select',
+                    	label: 'Scope',
+                    	items: [
+                    	        [ 'PAGE', 'PAGE' ],
+                    	        [ 'LOCAL', 'LOCAL' ]
+                	        ],
+                    	setup: function( widget ) {
+                    		this.setValue( widget.data.scope );
+                    	},
+                    	commit: function( widget ) {
+                    		widget.setData( 'scope', this.getValue() );
+                    	}
+                    },
+                    
+                    {
+                    	id: 'Start',
+                    	type: 'text',
+                    	label: 'Start',
+                    	width: '50px',
+                    	setup: function( widget ) {
+                    		this.setValue( widget.data.start );
+                    	},
+                    	commit: function( widget ) {
+                    		widget.setData( 'start', this.getValue() );
+                    	}
+                    }
+
                 ]
             }
         ]    
--- wiki-webapp/src/main/webapp/eXoPlugins/toc/plugin.js
+++ wiki-webapp/src/main/webapp/eXoPlugins/toc/plugin.js
@@ -18,16 +18,13 @@ CKEDITOR.plugins.add('toc', {
 				'div(!toc,align-left,align-right,align-center){width};' +
 				'div(!toc-content); h2(!toc-title)',
 			init: function() {
-				var width = this.element.getStyle( 'width' );
-				if ( width )
-				    this.setData( 'width', width );
-				if ( this.element.hasClass( 'align-left' ) )
-				    this.setData( 'align', 'left' );
-				if ( this.element.hasClass( 'align-right' ) )
-				    this.setData( 'align', 'right' );
-				if ( this.element.hasClass( 'align-center' ) )
-				    this.setData( 'align', 'center' );
-				this.hdata = 'aaaaa';
+				var tocDiv = this.element.find("div.toc-content").getItem(0);
+				if (tocDiv) {
+					this.data.depth = tocDiv.$.getAttribute('depth');
+					this.data.numbered = tocDiv.$.getAttribute('numbered');
+					this.data.scope = tocDiv.$.getAttribute('scope');
+					this.data.start = tocDiv.$.getAttribute('start');
+				}
 				//alert("cc");
 			},			
 			data: function() {
@@ -46,34 +43,109 @@ CKEDITOR.plugins.add('toc', {
 				var titleTags = { h1:0, h2:0, h3:0, h4:0, h5:0, h6:0 };
 				var range = new CKEDITOR.dom.range( editor.document );
 				var walker = new CKEDITOR.dom.walker( range );
-				var titles = [];
-				var content = "";
+				var headings = [];
+				//----------------------------------------------------
+				//get all headings
 				range.selectNodeContents( editor.document.getBody() );
-
 
 				walker.evaluator = function( node )
 				{
 					if ( node.getName
 						&& node.getName() in titleTags
 						&& CKEDITOR.tools.trim( node.getText() ) ) {
-							titles.push( node );
 //							alert(node.getName());
-							console.log(node.$.id);
+//							console.log(node.$.id);
 							if (node.$.id == undefined || node.$.id == "") {
 								count++;
 								node.$.id='heading' + count;
 							}
-							content += "<a href='#" + node.$.id + "'>" + node.$.innerHTML + "</a><br/>";
+							headings.push( node );
+//							content += "<a href='#" + node.$.id + "'>" + node.$.innerHTML + "</a><br/>";
 						}
 				};
-				
 				while ( ( next = walker.next() ) )
-					{}
+				{}
+				//----------------------------------------------------
+				/** build heading with content:
+				 * <ol>
+					 <li>test1
+					   <ol>
+					     <li>test3</li>
+					     <li>test4</li>
+					   </ol>
+					 </li>
+					 <li>test2></li>
+					</ol>
+				*/
+				var stack = [];
+				for (var i = 0; i < headings.length; i++) {
+					var currentNode = headings[i];
+					var currentArr = [];
+					currentArr.push(currentNode);
+					if (stack.length == 0) {
+						stack.push(currentArr);
+					} else if (stack[stack.length-1][0].getName() < currentNode.getName()){
+						stack.push(currentArr);
+					} else if (stack[stack.length-1][0].getName() == currentNode.getName()) {
+						stack[stack.length-1].push(currentNode);
+					} else {//stack[stack.length-1][0].getName() > currentNode.getName()
+						while (stack.length > 1 && stack[stack.length-1][0].getName() > currentNode.getName()) {
+							var prevArr = stack.pop();
+							var prevPrevArr = stack[stack.length-1];
+							var lastOfPrevPrev = prevPrevArr[prevPrevArr.length-1];
+							if (!lastOfPrevPrev.child) lastOfPrevPrev.child = [];
+							lastOfPrevPrev.child.addAll(prevArr);
+						}
+						stack[stack.length-1].push(currentNode);
+					}
+				}
+				
+				while (stack.length > 1) {
+					var prevArr = stack.pop();
+					var prevPrevArr = stack[stack.length-1];
+					var lastOfPrevPrev = prevPrevArr[prevPrevArr.length-1];
+					if (!lastOfPrevPrev.child) lastOfPrevPrev.child = [];
+					lastOfPrevPrev.child.addAll(prevArr);
+				}
+				
+				//var content = getHeadingWithIndents(headings);
+				var open = editor.widgets.toc && (editor.widgets.toc.data.numbered=="Yes")? "<ol>" : "<ul>"; 
+				var close = editor.widgets.toc && (editor.widgets.toc.data.numbered=="Yes")? "</ol>" : "</ul>";
+				var content = open;
+				var print = function(elem, indent) {
+					for (var i = 0; i < elem.length; i++) {
+						content +="<li><a href='#" + elem[i].$.id + "'>" + elem[i].$.innerHTML + "</a>";
+						console.log(indent + elem[i].getName());
+						if (elem[i].child) {
+							content += open;
+							print(elem[i].child, indent + " ");
+							content += close;
+						}
+						content+="</li>";
+					}
+				};
+				for (var i = 0; i < stack.length; i++) {
+					print(stack[i], "");
+				}
+				content += close;
+
+				//----------------------------------------------------
+				//set content and properties
 				//alert(titles.length);						
-				if (editor.widgets.toc && editor.widgets.toc.element.find(".toc-content p")
-				.getItem(0))
-				editor.widgets.toc.element.find(".toc-content p")
-				.getItem(0).setHtml(content);
+				if (editor.widgets.toc) {
+					var toc = editor.widgets.toc;
+					var tocContent = toc.element.find(".toc-content").getItem(0);
+					if (tocContent) {
+						tocContent.setHtml(content);
+					}
+					var tocDiv = toc.element.find("div.toc-content").getItem(0);
+					if (tocDiv) {
+						tocDiv.$.setAttribute('depth', toc.data.depth);
+						tocDiv.$.setAttribute('numbered', toc.data.numbered);
+						tocDiv.$.setAttribute('scope', toc.data.scope);
+						tocDiv.$.setAttribute('start', toc.data.start);
+					}
+				}
 			}
 			
 		});
